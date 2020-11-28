ba887958516c98292c95dee43623764ac9bb47af
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2020-11-28 19:16:46 +0530

    done ex03

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // alert("script works!!")
 
 
-var username = prompt("enter your name")
-alert("Welcome " + username )
+// var username = prompt("enter your name")
+// alert("Welcome " + username )
+
+var btnTranslate = document.querySelector("#translate");
+
+function clickHandler(){
+    console.log("button is clicked");
+}
+
+btnTranslate.addEventListener("click", clickHandler)
+
