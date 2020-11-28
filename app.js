7d9f5fd1673d2e215ab91b8b4aae12b82ad2ec16
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2020-11-28 21:06:14 +0530

    done ex05

--- app.js
+++ app.js
@@ -5,9 +5,14 @@
 // alert("Welcome " + username )
 
 var btnTranslate = document.querySelector("#translate");
+var txt = document.querySelector("#inputTxt");
+var outputDiv = document.querySelector("#output")
 
 function clickHandler(){
+    // document.body.style.backgroundColor = "blue";
+    console.log(txt.value);
     console.log("button is clicked");
+    outputDiv.innerText =  txt.value;
 }
 
 btnTranslate.addEventListener("click", clickHandler)
